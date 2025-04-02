421dea83ee07764316c8d8b97f95393bf9d59f53
Melvice Guimfack <melvice45@gmail.com>
2025-04-02 16:51:07 -0400

    update app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import LifeboatsChart from './components/lifeboats/LifeboatsChart'  
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,6 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <LifeboatsChart />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
