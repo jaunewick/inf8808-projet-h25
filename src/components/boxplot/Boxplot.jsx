6442e973a04ee715d36aadc8506f30e041bbcf51
Daniel Giao <daniel.giao@icloud.com>
2025-04-14 13:37:49 -0400

    Added boxplot analysis and Boxplot component cleanup

---
+++ src/components/boxplot/Boxplot.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import BoxplotSurvivalHeader from './utils/BoxplotSurvivalHeader'
+import BoxplotClassSurvivalHeader from './utils/BoxplotClassSurvivalHeader'
+import BoxplotPortClassSurvivalHeader from './utils/BoxplotPortClassSurvivalHeader'
+import BoxplotSurvival from './components/BoxplotSurvival'
+import BoxplotClassSurvival from './components/BoxplotClassSurvival'
+import BoxplotPortClassSurvival from './components/BoxplotPortClassSurvival'
+import BoxplotFooter from './utils/BoxplotFooter'
+
+function Boxplot({ data }) {
+    return (
+        <>
+            <BoxplotSurvivalHeader />
+            <BoxplotSurvival data={data} />
+            <BoxplotClassSurvivalHeader />
+            <BoxplotClassSurvival data={data} />
+            <BoxplotPortClassSurvivalHeader />
+            <BoxplotPortClassSurvival data={data} />
+            <BoxplotFooter />
+        </>
+    )
+}
+
+export default Boxplot
